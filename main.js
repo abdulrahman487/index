5cda0944866477301dea077df7d038a663f46bf4
abdulrahman487 <117461367+abdulrahman487@users.noreply.github.com>
2022-11-05 14:18:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// var row = document.getElementById('row');
+// var col = document.getElementById('col');
+    
+//     function createTable(row,col){
+//         var x,y = 1;
+        
+//         var output = "<table border='1'>";
+//         for(x = 1;x<=r;x++){
+//             // document.write(x+ " ");
+//             output = output+'<tr>';
+//             while(y<=c){
+//                 output = output+'<td>' + x*y+'</td>';
+//                 y+=1;
+//             }
+//             output=output+'</t>';
+//             y=1;
+//         }
+//         output= output+'</table>'
+//         document.write(output);
+//     }
+
+// function trigger(){
+//     document.getElementById('hover').addEventListener("mouseout",popup);
+//     function popup(){
+//         alert('welcome to my page');
+//     }
+// }
